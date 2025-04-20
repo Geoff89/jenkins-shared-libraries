1445aef71e1a881a88ab5c967e617fedf8fc1b6e
Geoffrey Sagini <saginigeoffrey@gmail.com>
2025-04-20 22:33:42 +0300

    Solving docker daemon socket issues

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,6 +1,6 @@
 def call(String aws_account_id, String region, String ecr_repoName) {
     sh """
-     docker run aquasec/trivy image ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest > scan.txt
+     docker run  -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy image ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest > scan.txt
      cat scan.txt
     """
 }
