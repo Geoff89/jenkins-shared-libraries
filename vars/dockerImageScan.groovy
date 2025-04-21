b9e559f7785a6daec2143fd5480d1e13e738d317
Geoffrey Sagini <saginigeoffrey@gmail.com>
2025-04-21 11:08:52 +0300

    Modified timeout in trivy command to increase timeout of scans

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,7 +1,7 @@
 def call(String aws_account_id, String region, String ecr_repoName) {
     sh """
      aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-     docker run  -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy image ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest > scan.txt
+     docker run  -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy image --timeout 30m ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest > scan.txt
      cat scan.txt
     """
 }
